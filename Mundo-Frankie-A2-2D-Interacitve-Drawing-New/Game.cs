583106dddb8f97c6006234a397d52a90a9cdb4de
FrankieMundo-MohawkCollege <frank.mundo3@mohawkcollege.ca>
2025-10-05 13:45:48 -0400

    Got half of the heart done time to rotate.

--- Mundo-Frankie-A2-2D-Interacitve-Drawing-New/Game.cs
+++ Mundo-Frankie-A2-2D-Interacitve-Drawing-New/Game.cs
@@ -12,6 +12,8 @@ namespace MohawkGame2D
     {
         // Place your variables here:
         Color White = new Color(255, 255, 255);
+        Color Red = new Color(255, 0, 0);
+
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -23,15 +25,36 @@ namespace MohawkGame2D
             Window.TargetFPS = 60;
         }
 
+       
         /// <summary>
         ///     Update runs every frame.
         /// </summary>
         public void Update()
         {
             Window.ClearBackground(White);
-
+            DrawHeart();
 
         }
-    }
 
+        void DrawHeart()
+        {
+            Draw.LineSize = 0;
+            Draw.LineColor = Color.Red;
+            Draw.FillColor = Color.Red;
+            Draw.Circle(200, 400, 55);
+
+            Draw.LineSize = 0;
+            Draw.LineColor = Color.Red;
+            Draw.FillColor = Color.Red;
+            Draw.Rectangle(145, 400, 110, 150);
+
+            Draw.LineSize = 0;
+            Draw.LineColor = Color.Red;
+            Draw.FillColor = Color.Red;
+            Draw.Rectangle(200, 400, 100, 150); 
+            
+        }
+    }
 }
+
+
