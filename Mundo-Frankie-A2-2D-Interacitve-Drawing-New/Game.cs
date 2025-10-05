e17cc90c8e041e486ada89863adfc9f97954eeb2
FrankieMundo-MohawkCollege <frank.mundo3@mohawkcollege.ca>
2025-10-04 20:03:09 -0400

    Setup and Background Completed.

---
+++ Mundo-Frankie-A2-2D-Interacitve-Drawing-New/Game.cs
@@ -0,0 +1,37 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+        Color White = new Color(255, 255, 255);
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("Frankie Mundo - Interactive 2D A2");
+            Window.SetSize(600, 800);
+            Window.TargetFPS = 60;
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            Window.ClearBackground(White);
+
+
+        }
+    }
+
+}
