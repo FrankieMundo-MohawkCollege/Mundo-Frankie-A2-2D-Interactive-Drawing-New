c3496a5726a7e09eda2f42fcc48deca6e7dff061
FrankieMundo-MohawkCollege <frank.mundo3@mohawkcollege.ca>
2025-10-05 19:07:23 -0400

    Final

--- Mundo-Frankie-A2-2D-Interacitve-Drawing-New/Game.cs
+++ Mundo-Frankie-A2-2D-Interacitve-Drawing-New/Game.cs
@@ -11,8 +11,17 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-        Color White = new Color(255, 255, 255);
-        Color Red = new Color(255, 0, 0);
+        Color White = new Color(225, 225, 225);
+        Color DarkBlue = new Color(0, 0, 139);
+        Color OffWhite = new Color(250, 249, 246);
+        Color DarkWhite = new Color(225, 217, 209);
+        Color BloodMoon = new Color(191, 58, 85);
+        Color BloodMoonDark = new Color(230, 106, 106);
+        Color CurrentMoonColor = new Color(250, 249, 246);
+        Color CurrentDarkMoonColor = new Color(225, 217, 209);
+        bool isBloodMoon = false;
+        int starX;
+        int starY;
 
 
         /// <summary>
@@ -23,38 +32,222 @@ namespace MohawkGame2D
             Window.SetTitle("Frankie Mundo - Interactive 2D A2");
             Window.SetSize(600, 800);
             Window.TargetFPS = 60;
-        }
+            Window.ClearBackground(DarkBlue);
+            for (int i = 0; i < 50; i++)
+            {
+                starX = Random.Integer(0, 600);
+                starY = Random.Integer(0, 800);
+                DrawStars();
+            }
 
+        }
+         
        
         /// <summary>
         ///     Update runs every frame.
         /// </summary>
         public void Update()
         {
-            Window.ClearBackground(White);
-            DrawHeart();
+            
+            DrawMoon();
+            
+            
+            if (Input.IsKeyboardKeyReleased(KeyboardInput.Space))
+            {
+                
+                if (isBloodMoon == false)
+                {
+                    CurrentMoonColor = BloodMoon;
+                    CurrentDarkMoonColor = BloodMoonDark;
+                    isBloodMoon = true;
+                }
+                else
+                {
+                    CurrentMoonColor = OffWhite;
+                    CurrentDarkMoonColor = DarkWhite;
+                    isBloodMoon = false;
+                }
 
-        }
-
-        void DrawHeart()
-        {
-            Draw.LineSize = 0;
-            Draw.LineColor = Color.Red;
-            Draw.FillColor = Color.Red;
-            Draw.Circle(200, 400, 55);
-
-            Draw.LineSize = 0;
-            Draw.LineColor = Color.Red;
-            Draw.FillColor = Color.Red;
-            Draw.Rectangle(145, 400, 110, 150);
-
-            Draw.LineSize = 0;
-            Draw.LineColor = Color.Red;
-            Draw.FillColor = Color.Red;
-            Draw.Rectangle(200, 400, 100, 150); 
+            }
             
         }
-    }
+
+
+        void DrawStars()
+        {
+            Draw.LineSize = 0;
+            Draw.FillColor = White;
+
+            Draw.Circle(starX, starY, 2);
+        }
+        void DrawMoon()
+        {
+            Draw.FillColor = CurrentMoonColor;
+            Draw.Circle(300, 400, 200);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = CurrentDarkMoonColor;
+            Draw.Circle(250, 350, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(210, 390, 30);
+
+            Draw.LineSize = 0;
+           
+            Draw.Circle(257, 300, 35);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(250, 400, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(270, 450, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(250, 350, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(290, 490, 20);
+
+            Draw.LineSize = 0;
+           
+            Draw.Circle(207, 250, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(250, 350, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(250, 350, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(450, 350, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(400, 490, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(428, 300, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(360, 360, 60);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(250, 560, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(360, 560, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(210, 488, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(180, 310, 20);
+
+            Draw.LineSize = 0;
+            
+            Draw.Circle(140, 400, 20);
+        }
+
+        void DrawBloodMoon()
+        {
+            Draw.FillColor = BloodMoon;
+            Draw.Circle(300, 400, 200);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(250, 350, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(210, 390, 30);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(257, 300, 35);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(250, 400, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(270, 450, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(250, 350, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(290, 490, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(207, 250, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(250, 350, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(250, 350, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(450, 350, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(400, 490, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(428, 300, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(360, 360, 60);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(250, 560, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(360, 560, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(210, 488, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(180, 310, 20);
+
+            Draw.LineSize = 0;
+            Draw.FillColor = BloodMoonDark;
+            Draw.Circle(140, 400, 20);
+
+
+
+        }
+
+
+      
+    }   
 }
-
-
